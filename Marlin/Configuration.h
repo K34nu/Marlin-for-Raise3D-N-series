c12f7689537d1ca39ecd64c5c58c9e119b94bc45
K34nu <contact@jl-designs.co.uk>
2020-08-20 11:52:08 +0100

    Update Configuration.h
    Fix min/max bed probe positions for compiling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1097,10 +1097,10 @@ const bool E1_LACK_ENDSTOP_INVERTING = true; // set to true to invert the logic 
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION MIN_PROBE_EDGE
-  #define RIGHT_PROBE_BED_POSITION (X_BED_SIZE - MIN_PROBE_EDGE)
-  #define FRONT_PROBE_BED_POSITION MIN_PROBE_EDGE
-  #define BACK_PROBE_BED_POSITION (Y_BED_SIZE - MIN_PROBE_EDGE)
+  #define LEFT_PROBE_BED_POSITION 43
+  #define RIGHT_PROBE_BED_POSITION (X_BED_SIZE - 15)
+  #define FRONT_PROBE_BED_POSITION 15
+  #define BACK_PROBE_BED_POSITION (Y_BED_SIZE - 50)
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
